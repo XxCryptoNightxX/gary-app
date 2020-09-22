da01ee79151737aa88b574e3477b5c8162229b8a
Guillaume Langelier <37124280+XxCryptoNightxX@users.noreply.github.com>
2020-09-22 12:00:48 -0400

    Initialize 9-use-state

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const initialData = [
+    "I change my locks every 16 days.",
+    "Creativity is for people with glasses who like to lie.",
+    "Cultivating a manly musk puts opponent on notice.",
+  ];
+
+  const [data, setData] = useState(initialData);
+
+  const listArray = data.map(item => {
+    return (<li>{item}</li>);
+  });
+
+  const clear = function () {
+    setData([]);
+  };
+
+  return (
+    <div className="App">
+      <h3>Quote Generator - React</h3>
+
+      <button type="button">Add Item</button>
+      <span>&nbsp;</span>
+      <button id="clear" type="button" onClick={clear}>Clear List</button>
+
+      <ul>
+        {listArray}
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
