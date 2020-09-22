44c08dec45ef78302a4bfd3a51b90b07fd200ad0
Guillaume Langelier <37124280+XxCryptoNightxX@users.noreply.github.com>
2020-09-22 12:00:41 -0400

    Initialize 2-add-buttons

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Quote Generator - React</h3>
+
+      <button type="button">Add Item</button>
+      <span>&nbsp;</span>
+      <button id="clear" type="button">Clear List</button>
+
+    </div>
+  );
+}
+
+export default App;
