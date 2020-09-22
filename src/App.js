fb07b7e749418cfbcf87b7a34c9f45427d507f73
Guillaume Langelier <37124280+XxCryptoNightxX@users.noreply.github.com>
2020-09-22 12:00:44 -0400

    Initialize 5-array-variable

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const listArray = [
+    <li>Some stuff</li>,
+    <li>Some more stuff</li>,
+    <li>Some more stuff again</li>,
+    <li>Even more stuff</li>,
+  ];
+
+
+  return (
+    <div className="App">
+      <h3>Quote Generator - React</h3>
+
+      <button type="button">Add Item</button>
+      <span>&nbsp;</span>
+      <button id="clear" type="button">Clear List</button>
+
+      <ul>
+        {listArray}
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
